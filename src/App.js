46e11098f171b7a80f80626a622b828b29f3bb99
Peder Goodman <peder.goodman@gmail.com>
2025-01-02 09:26:42 -0600

    add BrowserRouter to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './AppRouter';
 
 const App = () => {
   return (
     <>
       <h1>Hello</h1>
-      <AppRouter />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </>
   );
 };
