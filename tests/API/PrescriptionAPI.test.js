b1afa6221d823c8981fad584e1681ad1eaa614d1
j-odonnell <chokomo+github@proton.me>
2025-01-02 13:48:51 -0500

    define tests for getAllPrescriptions function

--- tests/API/PrescriptionAPI.test.js
+++ tests/API/PrescriptionAPI.test.js
@@ -7,12 +7,26 @@ import {
 	markPickedUp,
 } from '../../src/API/PrescriptionAPI';
 
+const requestSpy = jest.spyOn(RequestAPI, 'getRequest');
+
+afterEach(() => {
+	requestSpy.mockReset();
+});
+
 describe('getAllPrescriptions', () => {
 	it('should call RequestAPI.getRequest with correct args', async () => {
-		throw new Error();
+		await getAllPrescriptions();
+
+		expect(requestSpy).toHaveBeenCalledWith('/api/prescriptions');
 	});
 
 	it('should return result of RequestAPI.getRequest', async () => {
-		throw new Error();
+		const expectedResult = 'result';
+		RequestAPI.getRequest = jest
+			.fn()
+			.mockImplementationOnce(() => expectedResult);
+
+		const response = await getAllPrescriptions();
+		expect(response).toBe(expectedResult);
 	});
 });
