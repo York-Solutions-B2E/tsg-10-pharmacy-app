3b059522e746b37d37e8954418b8ea63f10580f1
j-odonnell <chokomo+github@proton.me>
2025-01-02 16:37:52 -0500

    implement NavBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import NavBar from './components/NavBar';
 import AppRouter from './AppRouter';
 
 const App = () => {
   return (
     <>
+      <CssBaseline />
+      <NavBar />
       <h1>Hello</h1>
       <AppRouter />
     </>
