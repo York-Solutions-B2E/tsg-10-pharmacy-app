613c5c97d8b40494a7537a89bf0f9bc80b95a18d
Peder Goodman <peder.goodman@gmail.com>
2025-01-02 09:25:21 -0600

    add color prop

--- src/Components/buttons/ButtonWithText.jsx
+++ src/Components/buttons/ButtonWithText.jsx
@@ -1,23 +1,40 @@
-import { Button } from "@mui/material";
+import { Button } from '@mui/material';
 
 const buttonStyling = {
-  margin: "15px 0",
+  margin: '15px 0',
 };
 
 /**
  * @param {Object} props
  * @param {() => void} props.onClick
  * @param {"text" | "contained" | "outlined"} [props.variant]
+ * @param {"primary" | "secondary" | "error" | "inherit" | "default" | "info" | "success" | "warning"} [props.color]
  * @param {boolean} [props.disabled]
  * @param {React.ReactNode} [props.startIcon]
  * @param {React.ReactNode} [props.endIcon]
  * @param {string} [props.buttonText]
  */
 const ButtonWithText = (props) => {
-  const { onClick, variant = "contained", disabled = false, buttonText = "click me", startIcon, endIcon } = props;
+  const {
+    onClick,
+    variant = 'contained',
+    color = 'primary',
+    disabled = false,
+    buttonText = 'click me',
+    startIcon,
+    endIcon,
+  } = props;
 
   return (
-    <Button sx={buttonStyling} variant={variant} color="primary" disabled={disabled} startIcon={startIcon} endIcon={endIcon} onClick={onClick}>
+    <Button
+      sx={buttonStyling}
+      variant={variant}
+      color={color}
+      disabled={disabled}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      onClick={onClick}
+    >
       {buttonText}
     </Button>
   );
