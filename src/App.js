a1fa45acec8f5f7376f6a9e44027a1fa210cfa1d
Peder Goodman <peder.goodman@gmail.com>
2025-01-02 10:31:51 -0600

    remove browser router from AppRouter, npm updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './AppRouter';
 
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <AppRouter />
     </>
   );
 };
