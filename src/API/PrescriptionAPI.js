b4af8d66645d28558d29acc88c2a9b07f5021b2f
j-odonnell <chokomo+github@proton.me>
2025-01-02 14:57:46 -0500

    implement fillPrescription function

--- src/API/PrescriptionAPI.js
+++ src/API/PrescriptionAPI.js
@@ -3,3 +3,17 @@ import RequestAPI from './RequestAPI';
 export const getAllPrescriptions = async () => {
 	return await RequestAPI.getRequest('/api/prescriptions');
 };
+
+export const fillPrescription = async (prescription) => {
+	try {
+		if (prescription === undefined) throw new Error('No prescription provided');
+		if (prescription === null) throw new Error('Provided prescription is null');
+		return await RequestAPI.putRequest(
+			`/api/prescriptions/${prescription.id}`,
+			JSON.stringify({ ...prescription, status: 'FILLED' })
+		);
+	} catch (error) {
+		console.error(error);
+		return { ok: false, status: 400, body: null };
+	}
+};
