291340865e6fd1c3533b1f8603dd1e0d895a001d
ethan-york-dev <egrahamc@yorksolutions.net>
2025-01-02 12:52:56 -0800

    Add to Router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,10 +1,11 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+import MedicationsPage from './pages/MedicationsPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/prescriptions" element={<div>prescriptions</div>} />
-      <Route path="/inventory" element={<div>inventory</div>} />
+      <Route path="/inventory" element={<MedicationsPage />} />
       <Route path="/orders" element={<div>orders</div>} />
       <Route path="/*" element={<Navigate to="/prescriptions" replace />} />
     </Routes>
