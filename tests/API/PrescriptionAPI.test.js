5b8a9ebf34d59f3258bd352a0308b9a39e233f94
j-odonnell <chokomo+github@proton.me>
2025-01-02 13:34:23 -0500

    stage tests for getAllPrescriptions function

---
+++ tests/API/PrescriptionAPI.test.js
@@ -0,0 +1,18 @@
+jest.mock('../../src/API/RequestAPI');
+
+import RequestAPI from '../../src/API/RequestAPI';
+import {
+	getAllPrescriptions,
+	fillPrescription,
+	markPickedUp,
+} from '../../src/API/PrescriptionAPI';
+
+describe('getAllPrescriptions', () => {
+	it('should call RequestAPI.getRequest with correct args', async () => {
+		throw new Error();
+	});
+
+	it('should return result of RequestAPI.getRequest', async () => {
+		throw new Error();
+	});
+});
