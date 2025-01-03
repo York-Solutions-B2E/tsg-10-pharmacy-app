c5b1cb26197844b4874cdf1fc67a80787ec7f46f
Peder Goodman <peder.goodman@gmail.com>
2025-01-03 14:11:17 -0600

    add assertion to App test

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,13 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import App from "../src/App";
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../src/App';
 
-describe("App", () => {
-  it("renders Hello heading", () => {
+describe('App', () => {
+  it('Should render the App Component', () => {
     render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
     );
+    const navBar = screen.getByTestId('navbar-test');
+    expect(navBar).toBeInTheDocument();
   });
 });
