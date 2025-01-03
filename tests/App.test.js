08b05e5186d0b67ea29067f8085a3fd8dd2bdbbc
j-odonnell <chokomo+github@proton.me>
2025-01-03 09:03:25 -0500

    suppress react-router-dom v6 warnings

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,14 +1,16 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import App from "../src/App";
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../src/App';
 
-describe("App", () => {
-  it("renders Hello heading", () => {
+describe('App', () => {
+  it('renders Hello heading', () => {
+    jest.spyOn(console, 'warn').mockImplementation();
     render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
     );
-    expect(screen.getByText("Hello")).toBeInTheDocument();
+    expect(screen.getByText('Hello')).toBeInTheDocument();
+    console.warn.mockRestore();
   });
 });
