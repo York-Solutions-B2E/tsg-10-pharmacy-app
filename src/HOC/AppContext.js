3406dc000a89ed83b576c3d56c39573f9370a4fa
Peder Goodman <peder.goodman@gmail.com>
2025-01-03 16:03:22 -0600

    hot fix, formatting and package-lock

--- src/HOC/AppContext.js
+++ src/HOC/AppContext.js
@@ -3,15 +3,15 @@ import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
 export const useAppContext = () => {
-	return useContext(AppContext);
+  return useContext(AppContext);
 };
 
 const AppProvider = ({ children }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<AppContext.Provider value={{ navigate }}>{children}</AppContext.Provider>
-	);
+  return (
+    <AppContext.Provider value={{ navigate }}>{children}</AppContext.Provider>
+  );
 };
 
 export default AppProvider;
