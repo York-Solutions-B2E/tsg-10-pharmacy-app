d5fdb8f9d628cf21021bb3f8666cc7791b9795c9
ethan-york-dev <egrahamc@yorksolutions.net>
2025-01-03 12:33:33 -0800

    Update MedicationsPage.js

--- src/pages/MedicationsPage.js
+++ src/pages/MedicationsPage.js
@@ -1,44 +1,45 @@
-import React, { useState } from "react";
-import MedicationsTable from "../components/MedicationsTable";
+import React, { useState } from 'react';
+import MedicationsTable from '../components/MedicationsTable';
 import { Box, Typography } from '@mui/material';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const MedicationsPage = () => {
   const [medications, setMedications] = useState([]);
 
-  const addMedication = (medication) => {
-    console.log("Add medication button clicked");
+  const orderMoreMedication = (medication) => {
+    console.log('Add medication button clicked');
   };
 
   const editMedicine = (id) => {
-    console.log("Edit medication button clicked");
+    console.log('Edit medication button clicked');
   };
 
   useEffect(() => {
-    const initialMedication = [{
-      id: 1,
-      nextDelivery: "2022-12-31",
-      code: "12345",
-      count: 30,
-      sufficiency: "In Stock",
-      name: "Medication 1",
-    },
-    {
-      id: 2,
-      nextDelivery: "2022-12-31",
-      code: "67890",
-      count: 0,
-      sufficiency: "Insufficient Stock",
-      name: "Medication 2"
-    },
-    {
-      id: 3,
-      nextDelivery: "2022-12-31",
-      code: "54321",
-      count: 0,
-      sufficiency: "On Order",
-      name: "Medication 3"
-    }
+    const initialMedication = [
+      {
+        id: 1,
+        nextDelivery: '2022-12-31',
+        code: '12345',
+        count: 30,
+        sufficiency: 'In Stock',
+        name: 'Medication 1',
+      },
+      {
+        id: 2,
+        nextDelivery: '2022-12-31',
+        code: '67890',
+        count: 0,
+        sufficiency: 'Insufficient Stock',
+        name: 'Medication 2',
+      },
+      {
+        id: 3,
+        nextDelivery: '2022-12-31',
+        code: '54321',
+        count: 0,
+        sufficiency: 'On Order',
+        name: 'Medication 3',
+      },
     ];
     setMedications(initialMedication);
   }, []);
@@ -50,7 +51,7 @@ const MedicationsPage = () => {
         <h5>These are all of the medications that we provide.</h5>
         <MedicationsTable
           medications={medications}
-          orderMore={addMedication}
+          orderMore={orderMoreMedication}
           editMedicine={editMedicine}
         />
       </Box>
