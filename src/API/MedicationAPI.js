91d59369f9698e883f99816b6d5608bf89e9f13d
j-odonnell <chokomo+github@proton.me>
2025-01-04 17:11:13 -0500

    implement adjustMedication function

--- src/API/MedicationAPI.js
+++ src/API/MedicationAPI.js
@@ -25,3 +25,21 @@ export const updateMedicationStock = async (inventory, updatedQuantity) => {
     return { ok: false, status: 400, body: null };
   }
 };
+
+export const adjustMedicationStock = async (inventory, stockAdjustment) => {
+  try {
+    if (inventory === undefined) throw new Error('No inventory provided');
+    if (inventory === null) throw new Error('Provided inventory is null');
+    if (inventory.id === null || isNaN(inventory.id) || inventory.id <= 0)
+      throw new Error('Inventory id must be a positive number');
+    if (stockAdjustment === null || isNaN(stockAdjustment))
+      throw new Error('Stock adjustment must be a number');
+    if (stockAdjustment === 0) return { ok: true, status: 200, body: null };
+    return await RequestAPI.putRequest(
+      `/api/inventory/${inventory.id}/adjust-stock/${stockAdjustment}`
+    );
+  } catch (error) {
+    console.error(error);
+    return { ok: false, status: 400, body: null };
+  }
+};
