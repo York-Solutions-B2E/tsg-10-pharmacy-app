433ca065d5982fe91a7c7e7f1eead7300bdfe1bd
j-odonnell <chokomo+github@proton.me>
2025-01-06 14:28:53 -0500

    define tests for getAllOrders function

--- tests/API/OrdersAPI.test.js
+++ tests/API/OrdersAPI.test.js
@@ -4,16 +4,25 @@ import RequestAPI from '../../src/API/RequestAPI';
 
 import { getAllOrders } from '../../src/API/OrdersAPI';
 
+const getRequestSpy = jest.spyOn(RequestAPI, 'getRequest');
+
 afterEach(() => {
   jest.resetAllMocks();
 });
 
 describe('getAllOrders', () => {
   it('should call RequestAPI.getRequest with correct args', async () => {
-    throw new Error();
+    await getAllOrders();
+
+    expect(getRequestSpy).toHaveBeenCalledWith('/api/orders');
   });
 
   it('should return result of RequestAPI.getRequest', async () => {
-    throw new Error();
+    const expectedResult = 'result';
+    getRequestSpy.mockImplementationOnce(() => expectedResult);
+
+    const response = await getAllOrders();
+
+    expect(response).toBe(expectedResult);
   });
 });
