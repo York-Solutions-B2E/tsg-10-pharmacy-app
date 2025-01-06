65621ad9525958f5468cfe8957b0cf95c26ffcb9
j-odonnell <chokomo+github@proton.me>
2025-01-06 16:31:23 -0500

    update placeOrder to use new validateOrder function

--- src/API/OrdersAPI.js
+++ src/API/OrdersAPI.js
@@ -32,27 +32,8 @@ export const getAllOrders = async () => {
 
 export const placeOrder = async (order) => {
   try {
-    if (order === undefined) throw new Error('Order cannot be undefined');
-    if (order === null) throw new Error('Order cannot be null');
-    if (
-      order.medicineId === undefined ||
-      isNaN(order.medicineId) ||
-      order.medicineId < 1
-    )
-      throw new Error('Medicine id must be a positive number');
-    if (
-      order.quantity === undefined ||
-      isNaN(order.quantity) ||
-      order.quantity < 1
-    )
-      throw new Error('Order quantity must be a positive number');
-    if (
-      order.deliveryDate === undefined ||
-      order.deliveryDate === null ||
-      !dayjs(order.deliveryDate).isValid() ||
-      dayjs().isAfter(order.deliveryDate)
-    )
-      throw new Error('Delivery date must be a date in the future');
+    const orderValid = validateOrder(order);
+    if (orderValid instanceof Error) throw orderValid;
     return await RequestAPI.postRequest('/api/orders', JSON.stringify(order));
   } catch (error) {
     console.error(error);
