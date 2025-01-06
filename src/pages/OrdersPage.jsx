183a88423555fd5db5a5436bcabbb286fbfb2abe
Peder Goodman <peder.goodman@gmail.com>
2025-01-06 16:24:31 -0600

    add form to orders page

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import ordersList from '../../dummy-data/order-list.json';
 import OrdersTable from '../components/data-display/OrdersTable';
+import OrderForm from '../components/forms/OrderForm';
 
 const OrdersPage = () => {
   const appointmentPageStyling = {
@@ -10,6 +11,7 @@ const OrdersPage = () => {
   return (
     <Box sx={appointmentPageStyling} data-testid="orders-page">
       <h1>OrdersPage</h1>
+      <OrderForm />
       <OrdersTable ordersList={ordersList} />
     </Box>
   );
