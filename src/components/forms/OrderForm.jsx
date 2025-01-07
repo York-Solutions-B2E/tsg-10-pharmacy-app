c09a30f19e252625196f4b260130a869cc6399bd
Peder Goodman <peder.goodman@gmail.com>
2025-01-07 13:11:18 -0600

    add NumberInput to order form, add validations for changes and submission

--- src/components/forms/OrderForm.jsx
+++ src/components/forms/OrderForm.jsx
@@ -1,8 +1,11 @@
 import { Autocomplete, Box } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 import inventoryList from '../../../dummy-data/inventory-list.json';
 import ButtonWithText from '../buttons/ButtonWithText';
+import NumberInput from './NumberInput';
 
 const containerStyling = {
   display: 'flex',
@@ -17,41 +20,126 @@ const containerStyling = {
 
 const OrderForm = () => {
   const [selectedMedicine, setSelectedMedicine] = useState(null);
-  const [inputValue, setInputValue] = useState('');
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(null);
+  const [deliveryDate, setDeliveryDate] = useState('');
 
+  const [inputValue, setInputValue] = useState('');
+  const [minQuantity, setMinQuantity] = useState(1);
+  const [quantityMessage, setQuantityMessage] = useState('');
+  const [numberError, setNumberError] = useState(false);
+
+  useEffect(() => {
+    // Set the default delivery date to 3 days from today
+    setDeliveryDate(dayjs().add(3, 'day'));
+  }, []);
+
+  // Format the options based on inventory for the Autocomplete component
   const formatOptions = inventoryList.map((item) => ({
     inventoryId: item.id,
     medicineId: item.medicine.id,
     label: `${item.medicine.name} (${item.medicine.code})`,
     medCode: item.medicine.code,
     medicine: item.medicine.name,
+    minQuantity: item.minimumOrderCount,
   }));
 
   // *** Click handlers ***
-  const handleSelectMedicine = () => {
-    console.log('Selected Medicine:', selectedMedicine);
-  };
-
-  const handleSelectQuantity = () => {
-    console.log('Selected Quantity:', quantity);
-  };
-
+  // Handles the text change in the Autocomplete field
   const handleInputValueChange = (event, newInputValue) => {
-    console.log('In handleInputValueChange, newInputValue:', newInputValue);
     setInputValue(newInputValue);
   };
 
+  // Handles the selection of a medicine in the Autocomplete component
   const handleChangeSelection = (event, newValue) => {
+    // clear the quantity and error message first
     setSelectedMedicine(newValue);
-    console.log('In onChange, newValue', newValue);
+    setMinQuantity(1);
+    setQuantityMessage(`Quantity must be at least ${1}`);
+    setNumberError(false);
+
+    // Set the quantity to 1 if the quantity is null
+    if (quantity === null) {
+      setQuantity(1);
+    }
+
+    // If the selected medicine is cleared, reset the quantity and minQuantity
+    if (newValue === null) {
+      setMinQuantity(1);
+      setQuantity(null);
+      setQuantityMessage('');
+    }
+
+    // Set the minQuantity and quantity message based on the selected medicine
+    if (newValue?.minQuantity) {
+      setMinQuantity(newValue?.minQuantity);
+      setQuantity(newValue?.minQuantity);
+      setQuantityMessage(`Quantity must be at least ${newValue?.minQuantity}`);
+    }
+  };
+
+  // Handler for NumberInput component
+  const handleChangeQuantity = (newQuantity) => {
+    setQuantity(newQuantity);
+    setNumberError(false);
+
+    // Check if the quantity is less than the minQuantity
+    if (newQuantity < minQuantity) {
+      setNumberError(true);
+    }
   };
 
   // *** Form submission ***
-  const handleOrderSubmit = () => {
+  const handleOrderSubmit = async () => {
     console.log('Clicked on Order Submit');
-    
-    console.log('Selected Medicine:', selectedMedicine);
+
+    // validation
+    if (!selectedMedicine) {
+      console.log('No medicine selected');
+      // TODO: Throw error message
+      return;
+    }
+
+    if (quantity < minQuantity || quantity === null) {
+      console.log('Quantity less than min');
+      setNumberError(true);
+      // TODO: Throw error message
+      return;
+    }
+
+    if (!deliveryDate) {
+      console.log('No delivery date selected');
+      // TODO: Throw error message
+      return;
+    }
+
+    if (deliveryDate < dayjs()) {
+      console.log('Delivery date is in the past');
+      // TODO: Throw error message
+      return;
+    }
+
+    // Prepare order data
+    const orderData = {
+      medicineId: selectedMedicine?.medicineId,
+      inventoryId: selectedMedicine?.inventoryId,
+      quantity,
+      deliveryDate,
+    };
+
+    console.log('Order Submitted. OrderData:', orderData);
+    // TODO: Send order data to the server
+    // const response = await placeOrder(orderData);
+    // if (response.status !== 201) {}
+    // TODO: trigger error message
+
+    // ? is success it 201?
+    // TODO: trigger success message
+    // if (response.status === 201) {}
+
+    // Reset the form on successful submission
+    setSelectedMedicine(null);
+    setQuantity(null);
+    setQuantityMessage('');
   };
 
   return (
@@ -69,7 +157,7 @@ const OrderForm = () => {
           handleInputValueChange(event, newInputValue);
         }}
         options={formatOptions}
-        renderInput={(params) => <TextField {...params} label="Controllable" />}
+        renderInput={(params) => <TextField {...params} label="Medicine" />}
         filterOptions={(options, { inputValue }) =>
           options.filter(
             (option) =>
@@ -78,11 +166,19 @@ const OrderForm = () => {
           )
         }
       />
-
-      <ButtonWithText
-        onClick={handleOrderSubmit}
-        buttonText="Order"
+      <NumberInput
+        placeholder="Select Quantity"
+        value={quantity}
+        onChange={(event, val) => handleChangeQuantity(val)}
+        min={minQuantity}
+        helperText={quantityMessage}
+        error={numberError}
       />
+      <Typography sx={{ textAlign: 'center' }}>
+        Expected delivery date: <br />{' '}
+        {dayjs(deliveryDate).format('MMM DD, YYYY')}
+      </Typography>
+      <ButtonWithText onClick={handleOrderSubmit} buttonText="Place Order" />
     </Box>
   );
 };
