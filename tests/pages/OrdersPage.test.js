1b269fbf3d2d2c08b037f3202434b5cf45f644a7
Peder Goodman <peder.goodman@gmail.com>
2025-01-08 11:27:52 -0600

    AppContext test working

--- tests/pages/OrdersPage.test.js
+++ tests/pages/OrdersPage.test.js
@@ -1,16 +1,81 @@
 import { cleanup, render, screen } from '@testing-library/react';
-import React from 'react';
+import MedicationAPI from '../../src/API/MedicationAPI';
+import OrdersAPI from '../../src/API/OrdersAPI';
+import { AppContext } from '../../src/HOC/AppContext';
 import OrdersPage from '../../src/pages/OrdersPage';
+import mockMedicationsList from '../__mocks__/mockMedicationsList';
+import mockOrdersList from '../__mocks__/mockOrdersList';
+console.log('AppContext in test:', AppContext);
+
+jest.mock('../../src/API/OrdersAPI');
+jest.mock('../../src/API/MedicationAPI');
 
 describe('OrdersPage', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    OrdersAPI.getAllOrders.mockResolvedValue({
+      status: 200,
+      body: mockOrdersList,
+    });
+
+    MedicationAPI.getAllMedications.mockResolvedValue({
+      status: 200,
+      body: mockMedicationsList,
+    });
+  });
+
   afterEach(() => {
     cleanup();
   });
 
-  test('renders OrdersTable', () => {
-    render(<OrdersPage />);
-    const ordersPage = screen.getByTestId('orders-page');
+  it('renders OrdersPage with initial state', async () => {
+    render(
+      <AppContext.Provider
+        value={{
+          ordersList: [],
+          updateOrders: jest.fn(),
+          medicationsList: [],
+          updateMedications: jest.fn(),
+          prescriptionsList: [],
+          updatePrescriptions: jest.fn(),
+          navigate: jest.fn(),
+        }}
+      >
+        <OrdersPage />
+      </AppContext.Provider>
+    );
 
-    expect(ordersPage).toBeInTheDocument();
+    expect(screen.getByTestId('orders-page')).toBeInTheDocument();
   });
+
+  // test('renders OrdersTable', () => {
+  //   renderWithProviders(<OrdersPage />);
+
+  //   const ordersPage = screen.getByTestId('orders-page');
+
+  //   expect(ordersPage).toBeInTheDocument();
+  // });
+
+  // it('fetches orders and medications on mount', async () => {
+  //   renderWithProviders(<OrdersPage />);
+
+  //   await waitFor(() => {
+  //     expect(ordersApi.getAllOrders).toHaveBeenCalledTimes(1);
+  //     expect(medicationsApi.getAllMedications).toHaveBeenCalledTimes(1);
+  //   });
+  // });
+
+  // it('updates context with fetched data', async () => {
+  //   renderWithProviders(<OrdersPage />);
+
+  //   await waitFor(() => {
+  //     expect(mockContextValues.updateOrders).toHaveBeenCalledWith(
+  //       mockOrdersList
+  //     );
+  //     expect(mockContextValues.updateMedications).toHaveBeenCalledWith(
+  //       mockMedicationsList
+  //     );
+  //   });
+  // });
 });
