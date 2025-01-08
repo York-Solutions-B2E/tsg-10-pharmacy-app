14bb15a20f72b4c5acf0114020a8d4d550d51170
Peder Goodman <peder.goodman@gmail.com>
2025-01-07 22:08:59 -0600

    update AppContext to set app data

--- src/HOC/AppContext.js
+++ src/HOC/AppContext.js
@@ -1,16 +1,42 @@
-import { createContext, useContext } from 'react';
+import { createContext, useCallback, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
+
 export const useAppContext = () => {
   return useContext(AppContext);
 };
 
 const AppProvider = ({ children }) => {
   const navigate = useNavigate();
+  const [ordersList, setOrdersList] = useState([]);
+  const [medicationsList, setMedicationsList] = useState([]);
+  const [prescriptionsList, setPrescriptionsList] = useState([]);
+
+  const updateOrders = useCallback((newOrders) => {
+    setOrdersList(newOrders);
+  }, []);
+
+  const updateMedications = useCallback((newMedications) => {
+    setMedicationsList(newMedications);
+  }, []);
+
+  const updatePrescriptions = useCallback((newPrescriptions) => {
+    setPrescriptionsList(newPrescriptions);
+  }, []);
+
+  const contextValue = {
+    navigate,
+    ordersList,
+    medicationsList,
+    prescriptionsList,
+    updateOrders,
+    updateMedications,
+    updatePrescriptions,
+  };
 
   return (
-    <AppContext.Provider value={{ navigate }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
   );
 };
 
