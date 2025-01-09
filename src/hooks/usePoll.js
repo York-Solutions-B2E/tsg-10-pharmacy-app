05de488c6f0334999a504f387e327deba7288430
j-odonnell <chokomo+github@proton.me>
2025-01-09 13:08:40 -0500

    remove unused import

--- src/hooks/usePoll.js
+++ src/hooks/usePoll.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 
 export const usePoll = (pollRequest, timeout) => {
   const [data, setData] = useState(null);
