de1c486186becedac95132bf71ecdf42f0f68c7a
j-odonnell <chokomo+github@proton.me>
2025-01-09 18:22:22 -0500

    add close, content, and action props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,9 +5,33 @@ import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const Modal = ({ children }) => {
-	return <Dialog open="true">{children}</Dialog>;
+const Modal = ({ children, open = false, close, content, action }) => {
+	return (
+		<Dialog open={open} sx={{ display: 'flex', justifyContent: 'center' }}>
+			{content?.type === 'http error' && (
+				<DialogTitle
+					variant="h3"
+					sx={{ display: 'flex', justifyContent: 'center', color: 'red' }}
+				>
+					{content?.status}
+				</DialogTitle>
+			)}
+			<DialogTitle>{content?.title}</DialogTitle>
+			<DialogContent>
+				{content?.body}
+				<Divider />
+				{content?.details}
+			</DialogContent>
+			{children}
+			<DialogActions>
+				<Button onClick={close}>close</Button>
+				<Box sx={{ flexGrow: '1' }} /> {/*empty spacer*/}
+				{action}
+			</DialogActions>
+		</Dialog>
+	);
 };
 
 export default Modal;
