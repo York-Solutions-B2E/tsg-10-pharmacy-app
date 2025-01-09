dbb440df228755d298cf50ec51dedcab2035e1e1
j-odonnell <chokomo+github@proton.me>
2025-01-09 11:44:15 -0500

    implement custom hook to make any poll request every given timeout

---
+++ src/hooks/usePoll.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useMemo } from 'react';
+
+export const usePoll = (pollRequest, timeout) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    let ignorePoll = false;
+    let ignoreTimer = false;
+
+    const poll = async (ignore) => {
+      const response = await pollRequest();
+      if (!ignore) setData(response);
+    };
+
+    poll(ignorePoll);
+
+    const timer = setInterval(
+      () => poll(ignoreTimer),
+      timeout ? timeout : 2000
+    );
+
+    return () => {
+      ignorePoll = true;
+      ignoreTimer = true;
+      clearInterval(timer);
+    };
+  }, []);
+
+  return { data };
+};
