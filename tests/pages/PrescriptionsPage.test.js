ac315a4e020e200c5ee1425b3743cc41ff411858
Peder Goodman <peder.goodman@gmail.com>
2025-01-08 18:23:42 -0600

    update PrescriptionsPage to use appcontext testing, TDD mocking expected function calls

--- tests/pages/PrescriptionsPage.test.js
+++ tests/pages/PrescriptionsPage.test.js
@@ -1,16 +1,65 @@
-import { cleanup, render, screen } from '@testing-library/react';
-import React from 'react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import PrescriptionAPI from '../../src/API/PrescriptionAPI';
+import * as appContext from '../../src/HOC/AppContext';
 import PrescriptionsPage from '../../src/pages/PrescriptionsPage';
+import mockPrescriptionsList from '../__mocks__/mockPrescriptionsList';
+
+jest.mock('../../src/API/PrescriptionAPI');
+
+const mockContextValues = {
+  ordersList: [],
+  medicationsList: [],
+  prescriptionsList: [],
+  updateMedications: jest.fn(),
+  updateOrders: jest.fn(),
+  updatePrescriptions: jest.fn(),
+};
 
 describe('PrescriptionsPage Tests', () => {
-  afterEach(() => {
-    cleanup();
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    jest
+      .spyOn(appContext, 'useAppContext')
+      .mockImplementation(() => mockContextValues);
+
+    PrescriptionAPI.getAllPrescriptions.mockResolvedValue({
+      status: 200,
+      body: mockPrescriptionsList,
+    });
   });
 
-  test('renders PrescriptionsPage', () => {
+  afterEach(() => {
+    cleanup();
+    appContext.useAppContext.mockReset();
+  });
+
+  afterAll(() => {
+    appContext.useAppContext.mockRestore();
+  });
+
+  it('should render PrescriptionsPage', () => {
     render(<PrescriptionsPage />);
     const prescriptionsPage = screen.getByTestId('prescriptions-page');
 
     expect(prescriptionsPage).toBeInTheDocument();
+  });
+
+  it('should call getAllPrescriptions on mount', async () => {
+    render(<PrescriptionsPage />);
+
+    await waitFor(() => {
+      expect(PrescriptionAPI.getAllPrescriptions).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should update the App context state with fetched data', async () => {
+    render(<PrescriptionsPage />);
+
+    await waitFor(() => {
+      expect(mockContextValues.updatePrescriptions).toHaveBeenCalledWith(
+        mockPrescriptionsList
+      );
+    });
   });
 });
