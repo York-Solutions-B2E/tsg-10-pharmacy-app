4b7e515c3482caadec69650419d0a861932a31c5
Peder Goodman <peder.goodman@gmail.com>
2025-01-08 19:14:26 -0600

    add getAllPrescriptions to Prescriptions Page

--- src/pages/PrescriptionsPage.jsx
+++ src/pages/PrescriptionsPage.jsx
@@ -1,14 +1,26 @@
 import { Box } from '@mui/material';
-import prescriptionsList from '../../dummy-data/prescription-list.json';
+import { useEffect } from 'react';
 import PrescriptionsTable from '../components/data-display/PrescriptionsTable';
+import { useAppContext } from '../HOC/AppContext';
+import { getAllPrescriptions } from '../API/PrescriptionAPI';
 
 const PrescriptionsPage = () => {
-  const appointmentPageStyling = {
-    padding: '30px 30px',
-  };
+  const { prescriptionsList, updatePrescriptions } = useAppContext();
+
+  useEffect(() => {
+    getAllPrescriptions()
+      .then((response) => {
+        updatePrescriptions(response.body);
+        console.log(response);
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
-    <Box sx={appointmentPageStyling} data-testid="prescriptions-page">
+    <Box sx={{ padding: '30px 30px' }} data-testid="prescriptions-page">
       <h1>PrescriptionsPage</h1>
       <PrescriptionsTable prescriptionsList={prescriptionsList} />
     </Box>
