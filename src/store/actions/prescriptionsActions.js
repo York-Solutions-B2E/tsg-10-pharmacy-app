2ca932edd8a366e5cd1a5869c818aefd6c4556a7
j-odonnell <chokomo+github@proton.me>
2025-01-13 20:22:17 +0000

    add async thunk for getAllActivePrescriptions

---
+++ src/store/actions/prescriptionsActions.js
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const getAllActivePrescriptions = createAsyncThunk(
+  'prescriptions/getAllActivePrescriptions',
+  async () => {
+    const response = await axios.get(
+      'http://localhost:8080/api/prescriptions/active'
+    );
+
+    console.log('prescriptions async thunk. response is: ', response);
+
+    return response;
+  }
+);
