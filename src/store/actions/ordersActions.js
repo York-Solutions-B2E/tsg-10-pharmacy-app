00ade9d6e567504fadcc8b0a205664bbf7cd3e16
Peder Goodman <peder.goodman@gmail.com>
2025-01-14 15:31:09 -0600

    add try catch to ordersActions to return backend api responses

--- src/store/actions/ordersActions.js
+++ src/store/actions/ordersActions.js
@@ -1,19 +1,34 @@
-import { getAllOrders } from '../../API/OrdersAPI';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const GET_ALL_ORDERS = gql`
+the query to get all orders
+`;
+
 export const getAllOrders = createAsyncThunk(
   'orders/getAllOrders',
-  async () => {
-    const response = await axios.post('http://localhost:8080/api/orders/');
-    
-    // getAllOrders();
+  async (args, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('http://localhost:8080/api/graphql', {
+        GET_ALL_ORDERS,
+      });
+      return response.data;
+    } catch (error) {
+      // console.error('Error in getting orders list:', error);
+      if (error.response) {
+        // Server responded with a status code outside the 2xx range
 
-    console.log('orders async thunk. response is:', response);
+        return rejectWithValue({
+          status: error.response.status,
+          body: error.response.data?.message, // Contains error response from the server
+        });
+      }
+    }
 
-    // JSON.parse(response.body);
-
-    return response;
-    // return JSON.parse(response.body);
+    // Server didn't respond
+    return rejectWithValue({
+      status: 500,
+      body: 'Server did not respond', // No response from the server
+    });
   }
 );
